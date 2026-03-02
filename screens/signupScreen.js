d2eec66f42d9621e135786a05f954b847f4bdcfb
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-02 17:44:14 +0100

    commit debug

--- screens/signupScreen.js
+++ screens/signupScreen.js
@@ -5,65 +5,45 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-<<<<<<< HEAD
-View,
-Platform
-=======
   View,
->>>>>>> origin/AstroBoussole
+  Platform,
 } from "react-native";
 import { useDispatch } from "react-redux";
-import {login} from '../reducers/user'
+import { login } from "../reducers/user";
 export default function SignupScreen() {
-
-
   const dispatch = useDispatch();
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
-<<<<<<< HEAD
+  const handleSubmit = () => {
+    if (email === "" || username === "" || password === "") return;
 
-
-  const handleSubmit =  () => {
-
-if ((email === "" || username === "" || password === "")) 
-return; 
-
-fetch(`http://192.168.1.22/users/signup`, {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
-},
-body: JSON.stringify({
-username: username,
-email: email,
-password: password,
-}),
-})
-.then((response) => response.json())
-.then((data) => { 
-dispatch(login({ token:data.token, username: data.username}))
-})
-};
-
-=======
-  const handlesubmit = () => {};
->>>>>>> origin/AstroBoussole
+    fetch(`http://192.168.1.22/users/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(login({ token: data.token, username: data.username }));
+      });
+  };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
       <View style={styles.container}>
         <Text style={styles.title}>Inscription</Text>
-<<<<<<< HEAD
-        <TextInput placeholder="Email" onChangeText={(value)=> setEmail(value)} value={email} style={styles.inputEmail} />
-<TextInput placeholder="username" onChangeText={(value) => setUsername(value)} value={username} style={styles.inputUsername}/>
-<TextInput placeholder="password" onChangeText={(value) => setPassword(value)} value ={password} style={styles.inputPassword}/>
-<TouchableOpacity onPress={(handleSubmit)}>
-<Text style={styles.textButton}>S'inscrire</Text>
-</TouchableOpacity>
-=======
         <TextInput
           placeholder="Email"
           onChangeText={(value) => setEmail(value)}
@@ -82,66 +62,35 @@ dispatch(login({ token:data.token, username: data.username}))
           value={password}
           style={styles.inputPassword}
         />
-        <TouchableOpacity onPress={handlesubmit}>
+        <TouchableOpacity onPress={handleSubmit}>
           <Text style={styles.textButton}>S'inscrire</Text>
         </TouchableOpacity>
->>>>>>> origin/AstroBoussole
       </View>
     </KeyboardAvoidingView>
   );
 }
 
-<<<<<<< HEAD
-const styles = StyleSheet.create ({
-container: {
-flex: 1,
-backgroundColor: "#ffffff",
-alignItems: 'center',
-justifyContent: 'center',
-
-},
-
-textButton: {
-fontSize: 24,
-color: "#5B8CFF",
-fontFamily: "Inter",
-
-},
-
-title: {
-color: "rgb(255, 116, 91)",
-fontSize: 24,
-},
-
-inputEmail: {
-backgroundcolor: "#ffffff",
-fontSize: 24,
-},
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-    alignItem: "center",
+    alignItems: "center",
     justifyContent: "center",
   },
+
+  textButton: {
+    fontSize: 24,
+    color: "#5B8CFF",
+    fontFamily: "Inter",
+  },
+
+  title: {
+    color: "rgb(255, 116, 91)",
+    fontSize: 24,
+  },
+
+  inputEmail: {
+    backgroundcolor: "#ffffff",
+    fontSize: 24,
+  },
 });
->>>>>>> origin/AstroBoussole
