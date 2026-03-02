a0adf08348386db45662a57595553688bf0527fa
Florian DCL <docruzeiro.f@gmail.com>
2026-03-02 13:49:55 +0000

    prettier

--- screens/signupScreen.js
+++ screens/signupScreen.js
@@ -5,7 +5,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-View,
+  View,
 } from "react-native";
 import { useDispatch } from "react-redux";
 
@@ -16,54 +16,43 @@ export default function signupScreen() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
-
-
-  const handlesubmit = () => {
-
-
-
-
-};
-
+  const handlesubmit = () => {};
 
   return (
     <KeyboardAvoidingView>
       <View style={styles.container}>
         <Text style={styles.title}>Inscription</Text>
-        <TextInput placeholder="Email" onChangeText={(value)=> setEmail(value)} value={email} style={styles.inputEmail} />
-<TextInput placeholder="username" onChangeText={(value) => setUsername(value)} value={username} style={styles.inputUsername}/>
-<TextInput placeholder="password" onChangeText={(value) => setPassword(value)} value ={password} style={styles.inputPassword}/>
-<TouchableOpacity onPress={(handlesubmit)}>
-<Text style={styles.textButton}>S'inscrire</Text>
-</TouchableOpacity>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(value) => setEmail(value)}
+          value={email}
+          style={styles.inputEmail}
+        />
+        <TextInput
+          placeholder="username"
+          onChangeText={(value) => setUsername(value)}
+          value={username}
+          style={styles.inputUsername}
+        />
+        <TextInput
+          placeholder="password"
+          onChangeText={(value) => setPassword(value)}
+          value={password}
+          style={styles.inputPassword}
+        />
+        <TouchableOpacity onPress={handlesubmit}>
+          <Text style={styles.textButton}>S'inscrire</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create ({
-container: {
-flex: 1,
-backgroundColor: "#ffffff",
-alignItem: 'center',
-justifyContent: 'center',
-
-}
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItem: "center",
+    justifyContent: "center",
+  },
+});
