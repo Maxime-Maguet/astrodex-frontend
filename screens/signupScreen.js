b555e7f54e13bc93543ce1b8399e0d1a0cd11438
Your name <Your email>
2026-03-03 12:31:00 +0100

    barre de  navigation

--- screens/signupScreen.js
+++ screens/signupScreen.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { useDispatch } from "react-redux";
 import { login } from "../reducers/user";
-export default function SignupScreen() {
+export default function SignupScreen({navigation}) {
   const dispatch = useDispatch();
 
   const [username, setUsername] = useState("");
@@ -20,7 +20,7 @@ export default function SignupScreen() {
   const handleSubmit = () => {
     if (email === "" || username === "" || password === "") return;
 
-    fetch(`http://192.168.1.22:3000/users/signup`, {
+    fetch(`http://192.168.1.34:3000/users/signup`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -33,8 +33,10 @@ export default function SignupScreen() {
     })
       .then((response) => response.json())
       .then((data) => { 
-        dispatch(login({ token: data.token, username: username }));
-      });
+        dispatch(login({ token: data.token, username: username })
+      ); console.log(data.username)
+      navigation.replace('TabNavigator');
+    });
   };
 
   return (
