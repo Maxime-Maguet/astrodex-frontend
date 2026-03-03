26e29ca1c28a675d3e62d2477c285d70db748074
Your name <Your email>
2026-03-03 15:15:52 +0100

    navigation resolu

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { Provider } from "react-redux";
 // import { PersistGate } from 'redux-persist/integration/react';
 import ObservationScreen from "./screens/ObservationScreen";
@@ -32,48 +32,47 @@ const store = configureStore({
 
 function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-         tabBarIcon: ({ color, size }) => {
-           let iconName = '';
-    
-           if (route.name === 'Observation') {
-             iconName = 'Binoculars';
-       //    } else if (route.name === 'Places') {
-         //    iconName = 'map-pin';
-           }
-    
-           return <FontAwesome name={iconName} size={size} color={color} />;
-         },
-         tabBarActiveTintColor: '#040c7c',
-         tabBarInactiveTintColor: '#ffffff',
-         headerShown: false,
-       })}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
+
+          if (route.name === "Observation") {
+            iconName = "Binoculars";
+            //    } else if (route.name === 'Places') {
+            //    iconName = 'map-pin';
+          }
+
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#040c7c",
+        tabBarInactiveTintColor: "#ffffff",
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="Observation" component={ObservationScreen} />
     </Tab.Navigator>
   );
 }
 
+//return (
+//<NavigationContainer>
+//<Stack.Navigator screenOptions={{ headerShown: false }}>
+
+//</Stack.Navigator>
+//</NavigationContainer>
+//);
+//<Stack.Screen name="Inscription" component={SignupScreen} />
 
 
-  //return (
-    //<NavigationContainer>
-      //<Stack.Navigator screenOptions={{ headerShown: false }}>
-      
-      
-      //<Stack.Screen name="Login" component={LoginScreen} />
-      
-      //</Stack.Navigator>
-      //</NavigationContainer>
-      //);
-      
-      
-      export default function App() {
-        return (
-          <Provider store={store}>
+
+export default function App() {
+  return (
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Inscription" component={SignupScreen} />
-      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- screens/signupScreen.js
+++ screens/signupScreen.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { useDispatch } from "react-redux";
 import { login } from "../reducers/user";
-export default function SignupScreen() {
+export default function SignupScreen({navigation}) {
   const dispatch = useDispatch();
 
   const [username, setUsername] = useState("");
@@ -20,7 +20,7 @@ export default function SignupScreen() {
   const handleSubmit = () => {
     if (email === "" || username === "" || password === "") return;
 
-    fetch(`http://192.168.1.22:3000/users/signup`, {
+    fetch(`http://192.168.1.34:3000/users/signup`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -34,8 +34,9 @@ export default function SignupScreen() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        if (data.token === true) {
+        if (data.token) {
           dispatch(login({ token: data.token, username: username }));
+          navigation.replace("TabNavigator")
         } else {
           console.log("utilisateur déjà existant.");
         }
