9983a8c9462c8becd97210a200cdacea86c83fef
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-03 17:43:45 +0100

    commit fin de journée j2

---
+++ reducers/astre.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const astreSlice = createSlice({
+  name: "astre",
+  initialState,
+  reducers: {},
+});
+
+export const {} = astreSlice.actions;
+export default astreSlice.reducer;
