e78a600875fc32f786849025a5872ebd246a88f7
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-04 13:15:27 +0100

    astrodex wip before lunch

---
+++ screens/AstrodexScreen.js
@@ -0,0 +1,71 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Image,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AddAstres } from "../reducers/astre";
+import AstroCard from "../components/AstroCard";
+import Header from "../components/Header";
+
+export default function AstrodexScreen() {
+  const [astres, setAstres] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("http://192.168.1.6:3000/astres")
+      .then((res) => res.json())
+      .then((astresData) => {
+        //console.log(astresData.astres[0].name);
+        if (astresData.result) {
+          setAstres(astresData.astres);
+          dispatch(AddAstres(astresData.astres));
+        }
+      });
+  }, []);
+
+  const astresList = astres.map((data, i) => {
+    console.log(data.rarity_level);
+
+    return (
+      <AstroCard
+        key={i}
+        name={data.name}
+        description={data.description}
+        imageUrl={data.imageUrl}
+        rarity={data.rarity_level}
+      />
+    );
+  });
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Header title="AstroDex" />
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        style={styles.scrollView}
+      >
+        {astresList}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#0B0F1A",
+  },
+  scrollView: {
+    flex: 1,
+    width: "100%",
+  },
+  scrollContent: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    alignItems: "center",
+  },
+});
