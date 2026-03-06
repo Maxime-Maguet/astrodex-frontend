81432613c1eaf83e210bc1d0155d7baef5d21b74
Florian DCL <docruzeiro.f@gmail.com>
2026-03-06 17:50:30 +0000

    commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -138,7 +138,7 @@ export default function HomeScreen() {
       <View style={styles.container}>
         <View style={styles.accueil}>
           <Text style={styles.accueil1}>Accueil</Text>
-          <View>
+          <View style={{ alignItems: "center" }}>
             {astroInfo && (
               <>
                 {/* <Text>{astroInfo.title}</Text> */}
@@ -146,11 +146,11 @@ export default function HomeScreen() {
                   source={{ uri: astroInfo.image }}
                   style={{ width: 100, height: 100 }}
                 />
-                <ScrollView>
+                {/* <ScrollView>
                   <Text style={styles.description}>
                     {astroInfo.description}
                   </Text>
-                </ScrollView>
+                </ScrollView> */}
               </>
             )}
           </View>
