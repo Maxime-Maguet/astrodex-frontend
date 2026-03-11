4211c886f9fc99e63de47b011e36796c65cff3de
Your name <Your email>
2026-03-11 12:25:24 +0100

    commit

--- components/homeAstresCard.js
+++ components/homeAstresCard.js
@@ -3,15 +3,14 @@ import { View, Text, StyleSheet, Image } from "react-native";
 export default function AstreCard(props) {
   return (
     <View style={styles.card}>
-      {props.validatedastre && (
-        <Text style={styles.astreValidated} >Déjà Capturé !</Text> //Props passé pour pouvoir afficher la phrase
-      )}
-      <Image source={{ uri: props.imageUrl }} style={styles.image} />
+    <Image source={{ uri: props.imageUrl }} style={styles.image} />
       <View style={styles.container}>
         <Text style={styles.astreName}>{props.name}</Text>
       </View>
-      
-      
+      {props.validatedastre && (
+        <Text style={styles.astreValidated} >Déjà Capturé !</Text> //Props passé pour pouvoir afficher la phrase
+      )}
+        
     </View>
   );
 }
@@ -29,7 +28,7 @@ const styles = StyleSheet.create({
     width: 110,
     height: 110,
     borderRadius: 55,
-    marginBottom: 10,
+    marginBottom: 5,
   },
 
   container: {
@@ -40,13 +39,14 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     color: "white",
-    marginBottom: 5,
+    marginBottom: 10,
     fontFamily: "Inter",
+    marginTop : -10,
   },
   astreValidated : {
-  fontSize: 15,
+  fontSize: 13,
   color: "white",
   fontWeight: "bold",
-  marginTop : -25,
+  marginTop : -15,
   }
   });
