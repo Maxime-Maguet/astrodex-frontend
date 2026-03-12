4b976364ae60b1b182cfbdb5c93dfba5a4b2b450
Allan <christoph.allan77120@gmail.com>
2026-03-12 15:58:52 +0100

    sauvegarde login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -11,7 +11,7 @@ import {
   Keyboard,
 } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector  } from "react-redux";
 import { login } from "../reducers/user";
 import { TouchableWithoutFeedback } from "react-native";
 import GradientImage from "../components/GradientImage";
