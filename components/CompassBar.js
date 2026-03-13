5465d8b1023d0c5c9f10ed251a78c692d352d859
Maxime-Maguet <maxime.maguet@gmail.com>
2026-03-13 16:45:14 +0100

    commentaire compassBar

--- components/CompassBar.js
+++ components/CompassBar.js
@@ -1,10 +1,16 @@
 import { StyleSheet, View, Text, Dimensions } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
+//Largeur de l'écran du téléphone pixels
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+//chaque degré = 4px sur l'écran -> augmenter cette valeur étale davantage la boussole
 const ZOOM_FACTOR = 4;
+
+//Largeur totale du ruban : 360° x 4px = 1440px (volontairement plus large que l'écran )
 const CONTENT_WIDTH = 360 * ZOOM_FACTOR;
 
+//les 8 points cardinaux avec leur position en degrés sur la boussole
 const MARKERS = [
   { label: "N", degree: 0 },
   { label: "NE", degree: 45 },
@@ -16,13 +22,20 @@ const MARKERS = [
   { label: "NW", degree: 315 },
 ];
 
+//props : degree(direction actuelle du téléphone), targetAzimut(direction de l'astre), isAligned (boolean)
 export default function CompassBar({ degree, targetAzimuth, isAligned }) {
+  //calcule le décalage horizontal du ruban pour centrer le degré actuel sur l'écran
+  // exemple: si on regarde vers le Nord(0°) -> translateX = SCREEN_WIDTH / 2 - 1440
   const translateX = SCREEN_WIDTH / 2 - degree * ZOOM_FACTOR - CONTENT_WIDTH;
 
   return (
     <View style={styles.container}>
+      {/* Ligne rouge fixe au centre de l'écran - indique la direction actuelle*/}
       <View style={styles.cursor} />
+
+      {/*Ruban de la boussole qui défile horizontalement selon translateX */}
       <View style={[styles.ribbon, { transform: [{ translateX }] }]}>
+        {/*répété 3 fois côté à côté pour que la boussole soit infinie et ne montre jamais de bord vide quand on tourne */}
         <CompassContent targetAzimuth={targetAzimuth} isAligned={isAligned} />
         <CompassContent targetAzimuth={targetAzimuth} isAligned={isAligned} />
         <CompassContent targetAzimuth={targetAzimuth} isAligned={isAligned} />
@@ -31,8 +44,10 @@ export default function CompassBar({ degree, targetAzimuth, isAligned }) {
   );
 }
 
+//sous composant : affiche les points cardinaux et l'icone de l'astre visé
 const CompassContent = ({ targetAzimuth, isAligned }) => (
   <View style={{ width: CONTENT_WIDTH, position: "relative", height: 80 }}>
+    {/* Affiche chaque point cardinal positionné en degree x  ZOOM_FACTOR pixels depuis la gauche. Le -10 centre le texte sur sa position*/}
     {MARKERS.map(({ label, degree }) => (
       <Text
         key={label}
@@ -42,6 +57,7 @@ const CompassContent = ({ targetAzimuth, isAligned }) => (
       </Text>
     ))}
 
+    {/*Affiche l'icône planète unqiuement si un astre est visé et qu'on est aligné. Positionnée à targerAzimuth × ZOOM_FACTOR, le -15 centre l'icône  */}
     {targetAzimuth !== null && isAligned && (
       <View style={[styles.target, { left: targetAzimuth * ZOOM_FACTOR - 15 }]}>
         <Text>
